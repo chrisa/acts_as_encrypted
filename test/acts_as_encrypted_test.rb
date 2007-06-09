6ce47eaefeea4b13049d800a17b5f57568c25bb4
chris <chris@0a37a38d-5523-0410-8951-9484fa28833c>
2007-06-09 12:17:32 +0000

    First real test for acts_as_encrypted.
    git-svn-id: http://sourcefarce.net/svn/rails/trunk/plugins/acts_as_encrypted@256 0a37a38d-5523-0410-8951-9484fa28833c

--- test/acts_as_encrypted_test.rb
+++ test/acts_as_encrypted_test.rb
@@ -1,8 +1,15 @@
+RAILS_ENV = 'test'
+require File.expand_path(File.join(File.dirname(__FILE__), '../../../../config/environment.rb'))
 require 'test/unit'
 
 class ActsAsEncryptedTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+  def test_creditcard
+    c = Creditcard.new
+    assert c
+    
+    c.ccnum = "123456781234abcd"
+    assert c.save
+
+    assert_equal "abcd", c.ccnum_lastfour
   end
 end
